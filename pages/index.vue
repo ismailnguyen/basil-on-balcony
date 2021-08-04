6f1631c9536fdff14c27d7cf9a3c4f29065c960b
Ismaïl NGUYEN <ismail.nguyen@loccitane.com>
2021-08-04 02:03:21 +0200

    add charts in dashboard

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,186 @@
 <template>
   <div v-if="!isLoading">
-    {{budgets.map(b => b.category)}}
+    <div class="section">
+  <div class="columns">
+    <aside class="column is-2">
+      <nav class="menu">
+        <p class="menu-label">
+          General
+        </p>
+        <ul class="menu-list">
+          <li><a class="is-active">Dashboard</a></li>
+          <li><a>Customers</a></li>
+        </ul>
+        <p class="menu-label">
+          Administration
+        </p>
+        <ul class="menu-list">
+          <li><a>Team Settings</a></li>
+          <li>
+            <a class="">Manage Your Team</a>
+            <ul>
+              <li><a>Members</a></li>
+              <li><a>Plugins</a></li>
+              <li><a>Add a member</a></li>
+            </ul>
+          </li>
+          <li><a>Invitations</a></li>
+          <li><a>Cloud Storage Environment Settings</a></li>
+          <li><a>Authentication</a></li>
+        </ul>
+        <p class="menu-label">
+          Transactions
+        </p>
+        <ul class="menu-list">
+          <li><a>Payments</a></li>
+          <li><a>Transfers</a></li>
+          <li><a>Balance</a></li>
+        </ul>
+      </nav>
+    </aside>
+    
+    <main class="column">
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <div class="title">Dashboard</div>
+          </div>
+        </div>
+        <div class="level-right">
+          <div class="level-item">
+            <button type="button" class="button is-small">
+              March 8, 2017 - April 6, 2017
+            </button>
+          </div>
+        </div>
+      </div>
+      
+      <div class="columns is-multiline">
+        <div class="column">
+          <div class="box">
+            <div class="heading">Top Seller Total</div>
+            <div class="title">56,950</div>
+            <div class="level">
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Sales $</div>
+                  <div class="title is-5">250,000</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Overall $</div>
+                  <div class="title is-5">750,000</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Sales %</div>
+                  <div class="title is-5">25%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="box">
+            <div class="heading">Revenue / Expenses</div>
+            <div class="title">55% / 45%</div>
+            <div class="level">
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Rev Prod $</div>
+                  <div class="title is-5">30%</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Rev Serv $</div>
+                  <div class="title is-5">25%</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Exp %</div>
+                  <div class="title is-5">45%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="box">
+            <div class="heading">Feedback Activity</div>
+            <div class="title">78% &uarr;</div>
+            <div class="level">
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Positive</div>
+                  <div class="title is-5">1560</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Negative</div>
+                  <div class="title is-5">368</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Pos/Neg %</div>
+                  <div class="title is-5">77% / 23%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="box">
+            <div class="heading">Orders / Returns</div>
+            <div class="title">75% / 25%</div>
+            <div class="level">
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Orders $</div>
+                  <div class="title is-5">425,000</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Returns $</div>
+                  <div class="title is-5">106,250</div>
+                </div>
+              </div>
+              <div class="level-item">
+                <div class="">
+                  <div class="heading">Success %</div>
+                  <div class="title is-5">+ 28,5%</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="columns is-multiline">
+        <div class="column is-6" v-for="(chart, index) in charts" :key="index">
+          <div class="panel">
+            <p class="panel-heading">
+              {{ chart.options.chart.title }}
+            </p>
+            <div class="panel-block">
+              <GChart
+                :type="chart.type"
+                :data="chart.data"
+                :options="chart.options"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</div>
   </div>
   <div v-else>
     <span>Chargement des articles en cours...</span>
@@ -8,7 +188,12 @@
 </template>
 
 <script>
+  import { GChart } from 'vue-google-charts'
+
   export default {
+    components: {
+      GChart
+    },
     data () {
       return {
         isLoading: true
@@ -19,10 +204,34 @@
 
       return {
         budgets: budgets,
-        isLoading: false
+        isLoading: false,
+        charts: [
+          {
+            type: 'PieChart',
+            data: [[ "Group", "Amount"]].concat(Array.from(budgets.filter(b => b.amount).map(b => ({ group: b.group, amount: b.amount})).reduce(
+  (m, {group, amount}) => m.set(group, (m.get(group) || 0) + amount), new Map
+), ([group, amount]) => ({group, amount})).map(b => [b.group, b.amount])),
+            options: {
+              chart: {
+                title: 'Budget groups',
+                subtitle: 'Groups repartitions'
+              },
+              is3D: true,
+            }
+          },
+          {
+            type: 'PieChart',
+            data: [[ "Category", "Amount"]].concat(budgets.filter(b => b.amount).map(b => [b.category, b.amount])),
+            options: {
+              chart: {
+                title: 'Budget categories',
+                subtitle: 'Categories repartition'
+              },
+              pieHole: 0.4
+            }
+          }
+        ]
       }
     },
-    components: {
-    }
   }
 </script>
